d5463e18871ccbcbb884300898b6e914bff2c8fc
rtorres <rene.torres@csps-efpc.gc.ca>
2022-06-30 15:40:13 -0400

    Add lots of text in about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,119 @@
-<script setup lang="ts">
-
-</script>
 <template>
   <div class="about">
+    <h1>About</h1>
+    <p>There will be a lot of text to test the iframe size on BS</p>
+    <h2>Long text begins now!!!</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur magni deserunt
+      similique! Quae earum cum veritatis iure voluptate, modi numquam, id illo dolores
+      aspernatur itaque, blanditiis minima animi consequuntur provident.
+      Vitae rem ab aperiam asperiores omnis culpa, beatae quod quas porro quam odio saepe
+      quis impedit alias ex ad laudantium illo velit consequatur ullam accusantium ratione
+      magni qui! Tenetur, quia!
+      Officia pariatur hic recusandae dolores, sunt cum laboriosam porro vero voluptatum,
+      nesciunt doloremque velit harum fugit reprehenderit, iste ipsam adipisci quia
+      provident. Veniam perferendis qui at magni adipisci cum cupiditate?
+      Dolore corrupti quasi hic debitis illo, dignissimos nobis deserunt tempore culpa
+      nisi sequi qui maxime veritatis doloremque quod consectetur veniam, cupiditate nam
+      earum ducimus odit tempora maiores vitae! Recusandae, ipsum.
+      Assumenda cupiditate excepturi quos incidunt neque aperiam ea quo nam perspiciatis,
+      amet corporis voluptate enim quod alias sequi iure laudantium officiis. Eius placeat
+      nobis neque quaerat voluptatem ab, blanditiis consequatur.
+      Libero ab ea, tempora odio quam soluta excepturi ipsum animi voluptatem facere
+      voluptate repellendus, accusantium doloremque dolor repellat culpa laborum
+      aspernatur dignissimos eligendi consectetur sint quisquam! Provident accusantium hic
+      quas?
+      Ullam magni dicta corrupti sed vero impedit distinctio unde ad, quasi mollitia, eius
+      cumque placeat, numquam recusandae dolores facere eligendi? Numquam rem quis quo
+      doloribus et culpa harum expedita nulla.
+      Illo repudiandae laborum eaque, deleniti et iste quae recusandae quo voluptate autem
+      quam eos provident iusto veritatis sit corrupti harum cum. Sapiente, nisi tenetur
+      est esse perferendis aperiam ut voluptatem!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur magni deserunt
+      similique! Quae earum cum veritatis iure voluptate, modi numquam, id illo dolores
+      aspernatur itaque, blanditiis minima animi consequuntur provident.
+      Vitae rem ab aperiam asperiores omnis culpa, beatae quod quas porro quam odio saepe
+      quis impedit alias ex ad laudantium illo velit consequatur ullam accusantium ratione
+      magni qui! Tenetur, quia!
+      Officia pariatur hic recusandae dolores, sunt cum laboriosam porro vero voluptatum,
+      nesciunt doloremque velit harum fugit reprehenderit, iste ipsam adipisci quia
+      provident. Veniam perferendis qui at magni adipisci cum cupiditate?
+      Dolore corrupti quasi hic debitis illo, dignissimos nobis deserunt tempore culpa
+      nisi sequi qui maxime veritatis doloremque quod consectetur veniam, cupiditate nam
+      earum ducimus odit tempora maiores vitae! Recusandae, ipsum.
+      Assumenda cupiditate excepturi quos incidunt neque aperiam ea quo nam perspiciatis,
+      amet corporis voluptate enim quod alias sequi iure laudantium officiis. Eius placeat
+      nobis neque quaerat voluptatem ab, blanditiis consequatur.
+      Libero ab ea, tempora odio quam soluta excepturi ipsum animi voluptatem facere
+      voluptate repellendus, accusantium doloremque dolor repellat culpa laborum
+      aspernatur dignissimos eligendi consectetur sint quisquam! Provident accusantium hic
+      quas?
+      Ullam magni dicta corrupti sed vero impedit distinctio unde ad, quasi mollitia, eius
+      cumque placeat, numquam recusandae dolores facere eligendi? Numquam rem quis quo
+      doloribus et culpa harum expedita nulla.
+      Illo repudiandae laborum eaque, deleniti et iste quae recusandae quo voluptate autem
+      quam eos provident iusto veritatis sit corrupti harum cum. Sapiente, nisi tenetur
+      est esse perferendis aperiam ut voluptatem!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur magni deserunt
+      similique! Quae earum cum veritatis iure voluptate, modi numquam, id illo dolores
+      aspernatur itaque, blanditiis minima animi consequuntur provident.
+      Vitae rem ab aperiam asperiores omnis culpa, beatae quod quas porro quam odio saepe
+      quis impedit alias ex ad laudantium illo velit consequatur ullam accusantium ratione
+      magni qui! Tenetur, quia!
+      Officia pariatur hic recusandae dolores, sunt cum laboriosam porro vero voluptatum,
+      nesciunt doloremque velit harum fugit reprehenderit, iste ipsam adipisci quia
+      provident. Veniam perferendis qui at magni adipisci cum cupiditate?
+      Dolore corrupti quasi hic debitis illo, dignissimos nobis deserunt tempore culpa
+      nisi sequi qui maxime veritatis doloremque quod consectetur veniam, cupiditate nam
+      earum ducimus odit tempora maiores vitae! Recusandae, ipsum.
+      Assumenda cupiditate excepturi quos incidunt neque aperiam ea quo nam perspiciatis,
+      amet corporis voluptate enim quod alias sequi iure laudantium officiis. Eius placeat
+      nobis neque quaerat voluptatem ab, blanditiis consequatur.
+      Libero ab ea, tempora odio quam soluta excepturi ipsum animi voluptatem facere
+      voluptate repellendus, accusantium doloremque dolor repellat culpa laborum
+      aspernatur dignissimos eligendi consectetur sint quisquam! Provident accusantium hic
+      quas?
+      Ullam magni dicta corrupti sed vero impedit distinctio unde ad, quasi mollitia, eius
+      cumque placeat, numquam recusandae dolores facere eligendi? Numquam rem quis quo
+      doloribus et culpa harum expedita nulla.
+      Illo repudiandae laborum eaque, deleniti et iste quae recusandae quo voluptate autem
+      quam eos provident iusto veritatis sit corrupti harum cum. Sapiente, nisi tenetur
+      est esse perferendis aperiam ut voluptatem!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur magni deserunt
+      similique! Quae earum cum veritatis iure voluptate, modi numquam, id illo dolores
+      aspernatur itaque, blanditiis minima animi consequuntur provident.
+      Vitae rem ab aperiam asperiores omnis culpa, beatae quod quas porro quam odio saepe
+      quis impedit alias ex ad laudantium illo velit consequatur ullam accusantium ratione
+      magni qui! Tenetur, quia!
+      Officia pariatur hic recusandae dolores, sunt cum laboriosam porro vero voluptatum,
+      nesciunt doloremque velit harum fugit reprehenderit, iste ipsam adipisci quia
+      provident. Veniam perferendis qui at magni adipisci cum cupiditate?
+      Dolore corrupti quasi hic debitis illo, dignissimos nobis deserunt tempore culpa
+      nisi sequi qui maxime veritatis doloremque quod consectetur veniam, cupiditate nam
+      earum ducimus odit tempora maiores vitae! Recusandae, ipsum.
+      Assumenda cupiditate excepturi quos incidunt neque aperiam ea quo nam perspiciatis,
+      amet corporis voluptate enim quod alias sequi iure laudantium officiis. Eius placeat
+      nobis neque quaerat voluptatem ab, blanditiis consequatur.
+      Libero ab ea, tempora odio quam soluta excepturi ipsum animi voluptatem facere
+      voluptate repellendus, accusantium doloremque dolor repellat culpa laborum
+      aspernatur dignissimos eligendi consectetur sint quisquam! Provident accusantium hic
+      quas?
+      Ullam magni dicta corrupti sed vero impedit distinctio unde ad, quasi mollitia, eius
+      cumque placeat, numquam recusandae dolores facere eligendi? Numquam rem quis quo
+      doloribus et culpa harum expedita nulla.
+      Illo repudiandae laborum eaque, deleniti et iste quae recusandae quo voluptate autem
+      quam eos provident iusto veritatis sit corrupti harum cum. Sapiente, nisi tenetur
+      est esse perferendis aperiam ut voluptatem!</p>
 
 
   </div>
 </template>
 
 <style scoped lang="scss">
+.about {
+  width: 80vw;
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: center;
+  align-items: center;
+}
 </style>
