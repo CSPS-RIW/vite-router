fc1015428c30db5e86a86e231879385e5b6c79e7
rtorres <rene.torres@csps-efpc.gc.ca>
2022-06-30 09:21:38 -0400

    #1 start adding content, localstorage works

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,29 +1,49 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 
 // reactive state
 const count = ref(0)
 const message = ref<string>()
 
 // functions that mutate state and trigger updates
-function increment() {
+const increment = () => {
   count.value++
 }
 
+const addMessage = () => {
+  console.log(message.value)
+}
+watch(message, newMessage => {
+  localStorage.setItem('messages', JSON.stringify(newMessage))
+  console.log(newMessage)
+
+}, { deep: true })
 // lifecycle hooks
 onMounted(() => {
-  // localStorage.setItem('InputMessage', )
+
+  // message.value = localStorage.getItem('message') || ''
 })
+
+
 </script>
 <template>
   <div class="about">
-    <h1>This is an about page with count of {{ count }}</h1>
+    <div class="live-heading" aria-live="polite">
+      <h1>This is an about page with count of {{ count }}</h1>
+    </div>
     <button class="btn btn-primary" @click="increment">Count</button>
-    <p class="mb-sm"><span class="instructions">Instructions:</span> Write something about
-      RIW</p>
-    <input v-model="message" placeholder="What do you think about RIW?"
-      class="custom-input" name="input-message">
-    <p class="mt-sm">{{ message }}</p>
+    <div class="activity">
+      <form action="POST" @submit.prevent="addMessage">
+        <h2>Instructions: Write something about
+          RIW</h2>
+        <input v-model="message" type="text" placeholder="What do you think about RIW?"
+          class="custom-input" name="input-message">
+      </form>
+      <p class="mt-sm">{{ message }}</p>
+
+      <button class="btn btn-secondary">Add message</button>
+    </div>
+
   </div>
 </template>
 
@@ -35,6 +55,9 @@ h1 {
 .btn {
   margin: 1rem;
   padding: 0.5rem 1rem;
+  height: 3rem;
+  // margin: auto;
+  font-size: 1.5rem;
   border: none;
   border-radius: 8px;
   cursor: pointer;
@@ -63,6 +86,17 @@ h1 {
   height: 3rem;
   font-size: 1.25rem;
   padding-left: 0.75rem;
+}
+
+.activity {
+  display: flex;
+  flex-flow: column wrap;
+  justify-items: flex-start;
+  align-content: center;
+
+  .btn {
+    margin-right: auto;
+  }
 }
 
 .mb-sm {
